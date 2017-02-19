cdf0eec818caa215d9d9d7da73575133b95fb3a9
Dmitry Scherbakov <3ddesign@bk.ru>
2017-02-19 17:51:47 +0200

    ToDoList 0.3

--- src/Todo.js
+++ src/Todo.js
@@ -2,32 +2,49 @@ import React, { Component } from 'react';
 import './App.css';
 import {Link} from 'react-router';
 
-let items = ['Test Task 1', 'Test Task 2'];
+var List = React.createClass({
+		getInitialState() {
+    		return {
+        	items: ['first', 'second']
+        };
+    },
+    //
+    // componentDidMount() {
+    // 		setTimeout(function () {
+    //         this.setState({
+    //             items: this.state.items.concat(['third'])
+    //         });
+    //     }.bind(this), 1000);
+    // },
 
-class List extends Component {
-    render () {
-        return (
-            <div className="tasks1">{items.map(item => <p className="tasks">{item}</p>)}</div>
-        );
+    // function press() {
+    //     this.setState({items: this.state.items.concat([document.getElementById("inp").value])});
+    // }
+
+    render: function() {
+        var stationComponents = this.state.items.map(function(item) {
+            return <p className="tasks">{item}</p>;
+        });
+        return <div>{stationComponents}</div>;
     }
-}
+});
 
-function press() {
-    items.push(document.getElementById("inp").value);
-}
 
 
 class Todo extends Component {
     render () {
-
-
+      function press() {
+        this.setState({
+            items: this.state.items.concat(['third'])
+        });
+      }
         return (
 
             <div className="App">
                 <h2>ToDo List</h2>
-
-                <List />
-                <form>
+               <div className="tasks1">
+                <List /></div>
+                <form>/
                     <label>
                         <input type="text" id="inp" className="item-value"/>
                     </label>
@@ -37,13 +54,10 @@ class Todo extends Component {
                 <p className="App-intro">
 
                     <Link to='/'>App</Link>
-
                 </p>
             </div>
         );
     }
 }
-
-
 
 export default Todo;
