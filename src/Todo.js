c492e846b80b0ba7147b8c29ee9e615e5da420f3
Dmitry Scherbakov <3ddesign@bk.ru>
2017-02-19 15:35:50 +0200

    ToDoList 0.3

--- src/Todo.js
+++ src/Todo.js
@@ -2,46 +2,48 @@ import React, { Component } from 'react';
 import './App.css';
 import {Link} from 'react-router';
 
-const items = ['Task 1', 'Task 2', 'Task 3', 'Task 3'];
+let items = ['Test Task 1', 'Test Task 2'];
 
-class TodoList extends Component {
-  render() {
-    return (
-        <div className="tasks1">{items.map(item => <p className="tasks">{item}</p>)}</div>
-    );
-  }
+class List extends Component {
+    render () {
+        return (
+            <div className="tasks1">{items.map(item => <p className="tasks">{item}</p>)}</div>
+        );
+    }
+}
+
+function press() {
+    items.push(document.getElementById("inp").value);
+    console.log(items);
+
 }
 
 
 class Todo extends Component {
-
-  press = function () {
-
-               }
+    render () {
 
 
-  render() {
-    return (
+        return (
 
-      <div className="App">
-          <h2>ToDo List</h2>
+            <div className="App">
+                <h2>ToDo List</h2>
 
-          <TodoList />
+                <List />
+                <form>
+                    <label>
+                        <input type="text" id="inp" className="item-value"/>
+                    </label>
+                    <input type="submit" value="Save" className="item-add" onClick={press}/>
 
-              <form>
-                <label>
-                <input type="text" name="name"  className="item-value"/>
-                </label>
-                <input type="submit" value="Save" className="item-add" onClick={this.press}/>
-              </form>
-        <p className="App-intro">
+                </form>
+                <p className="App-intro">
 
-          <Link to='/'>App</Link>
+                    <Link to='/'>App</Link>
 
-        </p>
-      </div>
-    );
-  }
+                </p>
+            </div>
+        );
+    }
 }
 
 
